19ecf632e4b5f631cc509fa90a5c9a894e50a772
timothyrd <timothyruizdocena@gmail.com>
2025-03-26 17:44:40 +0100

    First commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import chefMistral from "/src/assets/chef_mistral.png"
+
 export default function Header() {
     return (
         <header className="header">
             <div className="header-container">
-                <img className="header-img" src="../src/assets/chef_mistral.png"></img>
+                <img className="header-img" src={chefMistral}></img>
                 <span className="header-text">Chef Mistral</span>
             </div>
         </header>
